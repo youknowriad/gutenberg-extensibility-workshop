743631494e529022b324b2f2e41b337fe7fd913e
Riad Benguella <benguella@gmail.com>
2018-03-01 10:24:08 +0100

    Adding theme colors and wide alignments support

--- theme/functions.php
+++ theme/functions.php
@@ -20,3 +20,22 @@ function gutenbergtheme_editor_styles() {
 	wp_enqueue_style( 'gutenbergthemeblocks-style-blocks', get_template_directory_uri() . '/blocks.css');
 }
 add_action( 'enqueue_block_editor_assets', 'gutenbergtheme_editor_styles' );
+
+// Step 2: Adding theme support features
+function mytheme_setup_theme_supported_features() {
+	// No custom colors
+	add_theme_support( 'disable-custom-colors' );
+
+	// Define a palette
+	add_theme_support( 'editor-color-palette',
+			'#334499',
+			'#d0a5db',
+			'#eee',
+			'#444'
+	);
+
+	// Supports wide alignments
+	add_theme_support( 'align-wide' );
+}
+
+add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
