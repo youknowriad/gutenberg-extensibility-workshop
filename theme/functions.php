f3ed3aafa9654fd11a4fd6a6e00f8d926e7b555d
Riad Benguella <benguella@gmail.com>
2018-03-01 10:11:10 +0100

    Themes: Style blocks and editor

--- theme/functions.php
+++ theme/functions.php
@@ -6,6 +6,17 @@
 function gutenbergtheme_scripts() {
 	wp_enqueue_style( 'gutenbergtheme-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'gutenbergtheme-fonts', 'https://fonts.googleapis.com/css?family=Noto Serif:400,400italic,700,700italic&subset=latin,latin-ext', array(), null );
+
+	// Step 1: Adding block styles to the frontend
+	wp_enqueue_style( 'gutenbergthemeblocks-style-front', get_template_directory_uri() . '/blocks.css');
 }
 
 add_action( 'wp_enqueue_scripts', 'gutenbergtheme_scripts' );
+
+
+// Step 1: Adding block styles to the editor
+function gutenbergtheme_editor_styles() {
+	wp_enqueue_style( 'gutenbergthemeblocks-style-editor', get_template_directory_uri() . '/editor.css');
+	wp_enqueue_style( 'gutenbergthemeblocks-style-blocks', get_template_directory_uri() . '/blocks.css');
+}
+add_action( 'enqueue_block_editor_assets', 'gutenbergtheme_editor_styles' );
