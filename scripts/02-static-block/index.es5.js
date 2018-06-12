86a4352b1f7db045daed1d9fc202cb2cd4e4f3d1
Riad Benguella <benguella@gmail.com>
2018-06-12 15:46:30 +0100

    Adding es5 version for the static/advanced block examples

---
+++ scripts/02-static-block/index.es5.js
@@ -0,0 +1,47 @@
+var registerBlockType = wp.blocks.registerBlockType;
+var el = wp.element.createElement;
+
+registerBlockType("gew/static-block", {
+  title: "02 - Static Block",
+  icon: "welcome-learn-more",
+  category: "common",
+
+  // Attributes definition (how to parse the block attributes)
+  attributes: {
+    type: {
+      type: "string",
+      default: "danger"
+    },
+    message: {
+      type: "string",
+      source: "html",
+      selector: "p"
+    }
+  },
+
+  edit: function(props) {
+    var className = props.className;
+    var setAttributes = props.setAttributes;
+
+    // Extracting block attributes
+    var type = props.attributes.type;
+    var message = props.attributes.message;
+
+    // Event handler to update a block attribute
+    function updateMessage(event) {
+      setAttributes({ message: event.target.value });
+    }
+
+    return el(
+      "p",
+      { className: className + " alert-" + type },
+      el("textarea", { value: message, onChange: updateMessage })
+    );
+  },
+
+  save: function(props) {
+    var type = props.attributes.type;
+    var message = props.attributes.message;
+    return el("p", { className: "alert-" + type }, message);
+  }
+});
