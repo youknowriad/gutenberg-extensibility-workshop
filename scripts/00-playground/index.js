c6fc53baa925e57cf701072e918e67a3af377057
Andrew Duthie <andrew@andrewduthie.com>
2018-06-14 22:20:54 +0200

    Add playground block

---
+++ scripts/00-playground/index.js
@@ -0,0 +1,36 @@
+var registerBlockType = wp.blocks.registerBlockType;
+var el = wp.element.createElement;
+
+registerBlockType("gew/playground", {
+  // Title of the block.
+  title: "00 - Playground",
+
+  // Icon of the block dashicon string (https://developer.wordpress.org/resource/dashicons) or custom svg element.
+  icon: "welcome-learn-more",
+
+  // Category (common, formatting, layout, widgets, embed)
+  category: "common",
+
+  // Description of the block
+  description: "This block introduces you to block creation",
+
+  // Keywords to search for the block
+  keywords: ["learn", "gutenberg"],
+
+  // Block's editor representation
+  edit: function(props) {
+    return el("p", { className: props.className }, "Welcome to Gutenberg");
+  },
+
+  // Block's frontend representation
+  save: function() {
+    return el("p", {}, "Welcome to Gutenberg");
+  }
+});
+
+var registerPlugin = wp.plugins.registerPlugin;
+registerPlugin("gew/playground", {
+  render: function() {
+    return null;
+  }
+});
