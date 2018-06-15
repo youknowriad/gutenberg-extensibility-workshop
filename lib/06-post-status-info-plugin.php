a231795c58d1dc862563f863dfe2723859e45113
Andrew Duthie <andrew@andrewduthie.com>
2018-06-15 08:08:37 +0200

    Enqueue all scripts at admin enqueue actions

--- lib/06-post-status-info-plugin.php
+++ lib/06-post-status-info-plugin.php
@@ -1,11 +1,15 @@
 <?php
 
-wp_enqueue_script(
-	'gew-06-post-status-info-plugin',
-	gew_url( 'scripts/06-post-status-info-plugin/index.es5.js', __FILE__ ),
-	array(
-		'wp-edit-post',
-		'wp-element',
-		'wp-plugins',
-	)
-);
+function register_06_post_status_info_plugin() {
+	wp_enqueue_script(
+		'gew-06-post-status-info-plugin',
+		gew_url( 'scripts/06-post-status-info-plugin/index.es5.js', __FILE__ ),
+		array(
+			'wp-edit-post',
+			'wp-element',
+			'wp-plugins',
+		)
+	);
+}
+
+add_action( 'admin_enqueue_scripts', 'register_06_post_status_info_plugin' );
--- lib/07-publish-panel-plugin.php
+++ lib/07-publish-panel-plugin.php
@@ -1,11 +1,15 @@
 <?php
 
-wp_enqueue_script(
-	'gew-07-publish-panel-plugin',
-	gew_url( 'scripts/07-publish-panel-plugin/index.es5.js', __FILE__ ),
-	array(
-		'wp-edit-post',
-		'wp-element',
-		'wp-plugins',
-	)
-);
+function register_07_publish_panel_plugin() {
+	wp_enqueue_script(
+		'gew-07-publish-panel-plugin',
+		gew_url( 'scripts/07-publish-panel-plugin/index.es5.js', __FILE__ ),
+		array(
+			'wp-edit-post',
+			'wp-element',
+			'wp-plugins',
+		)
+	);
+}
+
+add_action( 'admin_enqueue_scripts', 'register_07_publish_panel_plugin' );
